930d72ccfe6ca29e45d97bc6a1df5da6e918077c
kismet <ZhangHY1012@gmail.com>
2019-07-16 16:34:13 +0900

    me

---
+++ decision_tree/regression.py
@@ -0,0 +1,138 @@
+import pandas as pd
+import numpy as np
+from IPython.display import display
+from fastai.imports import *
+from sklearn import metrics
+import pdb
+
+class DecisionTree():
+    def __init__(self, x, y, idxs = None, min_leaf=2):
+        global my_n
+        print("create my_node",my_n)
+        my_n = my_n+1
+        if idxs is None: idxs=np.arange(len(y))
+        self.x,self.y,self.idxs,self.min_leaf = x,y,idxs,min_leaf
+        self.n,self.c = len(idxs), x.shape[1]
+        self.val = np.mean(y[idxs])
+        self.score = float('inf')
+        self.find_varsplit()
+
+    def find_varsplit(self):
+        for i in range(self.c): self.find_better_split(i)
+        if self.score == float('inf'): return
+        x = self.split_col
+        pdb.set_trace()
+        lhs = np.nonzero(x<=self.split)[0]
+        rhs = np.nonzero(x>self.split)[0]
+        self.lhs = DecisionTree(self.x, self.y, self.idxs[lhs])
+        self.rhs = DecisionTree(self.x, self.y, self.idxs[rhs])
+
+    def find_better_split(self, var_idx):
+        pdb.set_trace()
+        x,y = self.x.values[self.idxs,var_idx], self.y[self.idxs]
+        sort_idx = np.argsort(x)
+        pdb.set_trace()
+        sort_y,sort_x = y[sort_idx], x[sort_idx]
+        rhs_cnt,rhs_sum,rhs_sum2 = self.n, sort_y.sum(), (sort_y**2).sum()
+        lhs_cnt,lhs_sum,lhs_sum2 = 0,0.,0.
+
+        for i in range(0,self.n-self.min_leaf-1):
+            xi,yi = sort_x[i],sort_y[i]
+            lhs_cnt += 1; rhs_cnt -= 1
+            lhs_sum += yi; rhs_sum -= yi
+            lhs_sum2 += yi**2; rhs_sum2 -= yi**2
+            if i<self.min_leaf or xi==sort_x[i+1]:
+                continue
+            lhs_std = std_agg(lhs_cnt, lhs_sum, lhs_sum2)
+            rhs_std = std_agg(rhs_cnt, rhs_sum, rhs_sum2)
+            curr_score = lhs_std*lhs_cnt + rhs_std*rhs_cnt
+            if curr_score<self.score:
+                self.var_idx,self.score,self.split = var_idx,curr_score,xi
+
+    @property
+    def split_name(self): return self.x.columns[self.var_idx]
+
+    @property
+    def split_col(self): return self.x.values[self.idxs,self.var_idx]
+
+    @property
+    def is_leaf(self): return self.score == float('inf')
+
+    def __repr__(self):
+        s = f'n: {self.n}; val:{self.val}'
+        if not self.is_leaf:
+            s += f'; score:{self.score}; split:{self.split}; var:{self.split_name}'
+        return s
+
+    def predict(self, x):
+        return np.array([self.predict_row(xi) for xi in x])
+
+    def predict_row(self, xi):
+        if self.is_leaf: return self.val
+        t = self.lhs if xi[self.var_idx]<=self.split else self.rhs
+        return t.predict_row(xi)
+
+# Data simulation
+
+x = np.arange(0,50)
+x = pd.DataFrame({'x':x})
+
+# just random uniform distributions in differnt range
+
+y1 = np.random.uniform(10,15,10)
+y2 = np.random.uniform(20,25,10)
+y3 = np.random.uniform(0,5,10)
+y4 = np.random.uniform(30,32,10)
+y5 = np.random.uniform(13,17,10)
+
+y = np.concatenate((y1,y2,y3,y4,y5))
+y = y[:,None]
+
+def std_agg(cnt, s1, s2): return math.sqrt((s2/cnt) - (s1/cnt)**2)
+
+
+
+
+xi = x # initialization of input
+yi = y # initialization of target
+# x,y --> use where no need to change original y
+ei = 0 # initialization of error
+n = len(yi)  # number of rows
+predf = 0 # initial prediction 0
+
+for i in range(1): # like n_estimators
+    my_n = 0
+    tree = DecisionTree(xi,yi)
+    tree.find_better_split(0)
+
+    r = np.where(xi == tree.split)[0][0]
+
+    left_idx = np.where(xi <= tree.split)[0]
+    right_idx = np.where(xi > tree.split)[0]
+
+    predi = np.zeros(n)
+    np.put(predi, left_idx, np.repeat(np.mean(yi[left_idx]), r))  # replace left side mean y
+    np.put(predi, right_idx, np.repeat(np.mean(yi[right_idx]), n-r))  # right side mean y
+
+    predi = predi[:,None]  # make long vector (nx1) in compatible with y
+    predf = predf + predi  # final prediction will be previous prediction value + new prediction of residual
+
+    ei = y - predf  # needed originl y here as residual always from original y
+    yi = ei # update yi as residual to reloop
+    print("time seperation marker",my_n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
